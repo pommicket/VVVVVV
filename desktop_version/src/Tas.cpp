36be66c0eb9862fe44d7b3d278fba72f7467cbe7
pommicket <pommicket@gmail.com>
2023-03-09 20:48:34 -0500

    figured out what to do!

--- desktop_version/src/Tas.cpp
+++ desktop_version/src/Tas.cpp
@@ -16,17 +16,6 @@ typedef struct {
 } State;
 
 static State save;
-static const char empty_string[] = "";
-
-// like strdup but returns NULL if s is NULL,
-// and does not allocate if s is an empty string
-// (as a result yoiu cant modify the resulting string and you shouldnt free it
-//  yes this may cause memmory leak over long period of time)
-static const char *const_strdup(const char *s) {
-	if (!s) return NULL;
-	if (!*s) return empty_string;
-	return SDL_strdup(s);
-}
 
 static void copy_state_from_game(State *to) {
 	to->graphics = graphics;
@@ -36,11 +25,6 @@ static void copy_state_from_game(State *to) {
 	to->map = map;
 	to->obj = obj;
 	to->gameScreen = gameScreen;
-	
-	mapclass *map = &to->map;
-	// leaky time!!
-	map->roomname = const_strdup(map->roomname);
-	map->hiddenname = const_strdup(map->hiddenname);
 }
 
 static void copy_state_to_game(const State *state) {
@@ -51,6 +35,7 @@ static void copy_state_to_game(const State *state) {
 	map = state->map;
 	obj = state->obj;
 	gameScreen = state->gameScreen;
+	map.gotoroom(game.roomx, game.roomy);
 }
 
 void save_state(void) {
